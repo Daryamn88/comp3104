37895060b18e5e4c4bcd438f103f5c201c70d070
Darya.M <darya.mansouri14@gmail.com>
2024-10-11 19:59:28 -0400

    Added Tittle

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101394900 || Darya Mansouri || DevOps</h5>
+      <h5>Developed By: 101394900 || Darya Mansouri || DevOps - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
